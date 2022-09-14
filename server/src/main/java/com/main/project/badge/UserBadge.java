b147e09b07972d74350dafd56f07b5ecc7a476e9
Nanninggu <[pohd8494@naver.com]>
2022-09-14 14:05:45 +0900

    feat/client 디렉토리 추가

---
+++ server/src/main/java/com/main/project/badge/UserBadge.java
@@ -0,0 +1,26 @@
+package com.main.project.badge;
+
+import com.main.project.user.WebUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class UserBadge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userBadgeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_Id")
+    private WebUser webUser;
+
+    @ManyToOne
+    @JoinColumn(name = "badge_Id")
+    private Badge badge;
+
+}
