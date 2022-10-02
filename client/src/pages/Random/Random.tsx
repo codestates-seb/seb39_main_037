20eb6adc8fd355cb5df96b3987c7ccf26ec2948e
jin92926 <jin92926@gmail.com>
2022-10-02 19:26:49 +0900

    random page in production

--- client/src/pages/Random/Random.tsx
+++ client/src/pages/Random/Random.tsx
@@ -1,5 +1,89 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import { RandomMockupData } from "./RandomMockupData";
+
 const Random = () => {
-  return <div>Random</div>;
+  const [isClick, setIsClick] = useState<boolean>(false);
+  const [randomMenu, setRandomMenu] = useState("");
+
+  const ClickRandom = (e: React.MouseEvent<HTMLElement>) => {
+    e.stopPropagation();
+    setIsClick(true);
+    const randomRecommand =
+      RandomMockupData[Math.floor(Math.random() * RandomMockupData.length)];
+    setRandomMenu(randomRecommand.menu);
+  };
+
+  const allnum = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+  const newnum = [];
+
+  while (allnum.length - 7) {
+    const movenum = allnum.splice(
+      Math.floor(Math.random() * allnum.length),
+      1,
+    )[0];
+    newnum.push(movenum);
+  }
+  console.log(newnum);
+
+  return (
+    <Content>
+      <DivBox>
+        {RandomMockupData.map(({ menu, menuid }) => (
+          <div key={menuid}>{menu}</div>
+        ))}
+      </DivBox>
+      <div>
+        <div>
+          <button type="button" onClick={ClickRandom}>
+            랜덤 추천
+          </button>
+        </div>
+        <ul>
+          <li>
+            {isClick === false ? (
+              <button type="button">메뉴</button>
+            ) : (
+              <button type="button">{randomMenu}</button>
+            )}
+          </li>
+          <li>
+            {isClick === false ? (
+              <button type="button">메뉴</button>
+            ) : (
+              <button type="button">{randomMenu}</button>
+            )}
+          </li>
+          <li>
+            {isClick === false ? (
+              <button type="button">메뉴</button>
+            ) : (
+              <button type="button">{randomMenu}</button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </Content>
+  );
 };
 
 export default Random;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+const DivBox = styled.div`
+  border: 1px solid #e5e5e5;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1rem;
+  gap: 3rem;
+  max-width: 560px;
+  min-width: 290px;
+`;
---
+++ client/src/pages/Random/RandomMockupData.tsx
@@ -0,0 +1,62 @@
+export const RandomMockupData = [
+  {
+    menuid: 1,
+    menu: "음식 메뉴1",
+  },
+  {
+    menuid: 2,
+    menu: "음식 메뉴2",
+  },
+  {
+    menuid: 3,
+    menu: "음식 메뉴3",
+  },
+  {
+    menuid: 4,
+    menu: "음식 메뉴4",
+  },
+  {
+    menuid: 5,
+    menu: "음식 메뉴5",
+  },
+  {
+    menuid: 6,
+    menu: "음식 메뉴6",
+  },
+  {
+    menuid: 7,
+    menu: "음식 메뉴7",
+  },
+  {
+    menuid: 8,
+    menu: "음식 메뉴8",
+  },
+  {
+    menuid: 9,
+    menu: "음식 메뉴9",
+  },
+  {
+    menuid: 10,
+    menu: "음식 메뉴10",
+  },
+  {
+    menuid: 11,
+    menu: "음식 메뉴11",
+  },
+  {
+    menuid: 12,
+    menu: "음식 메뉴12",
+  },
+  {
+    menuid: 13,
+    menu: "음식 메뉴13",
+  },
+  {
+    menuid: 14,
+    menu: "음식 메뉴14",
+  },
+  {
+    menuid: 15,
+    menu: "음식 메뉴15",
+  },
+];
